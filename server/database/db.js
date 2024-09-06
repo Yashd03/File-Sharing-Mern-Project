79997156b254fe3e223b2c88bb0ca88eab70bf29
Yash Dubey <113511452+Yashd03@users.noreply.github.com>
2024-09-06 13:45:33 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI='mongodb+srv://Yashd03:Yash123@cluster0.ykwyu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const MONGODB_URI='mongodb+srv://Yashd03:Password@cluster0.ykwyu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 const DBConnection=async()=>{
     try {
         await mongoose.connect(MONGODB_URI,{useNewUrlParser: true});
